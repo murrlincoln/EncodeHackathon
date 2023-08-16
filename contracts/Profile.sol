8c1ee961a23b19c54f8092594ec70e7c65a1e7c5
justinyeh1995 <justinyeh1995@gmail.com>
2023-08-16 11:32:28 +0800

    filling template

--- contracts/Profile.sol
+++ contracts/Profile.sol
@@ -3,14 +3,27 @@ pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
+import "./Post.sol";
 
 contract Profile is ERC721, Ownable {
+    struct Post {
+        uint256 id;
+        string contentCID;
+    }
+    struct Connection {
+        address profileAddress;
+        bool approved;
+    }
     string public profileName;
     string public profileDescription;
     string public profilePictureLink;
+    uint256 public postCount;
+    uint256 public connectionCount;
+
 
     address[] public owners;
     mapping(address => bool) public isOwner;
+    mapping(uint256 => Post) private tokenIdToPost;
 
     constructor(
         string memory _name,
@@ -62,24 +75,33 @@ contract Profile is ERC721, Ownable {
     function post(string memory _contentCID) external onlyOwners {
         // Implementation to create NFT from the content CID
         // Mint NFT and assign it to this contract
+        // Increment postCount
+        postCount++;
+        _safeMint(address(this), postCount);
+        tokenIdToPost[postCount] = Post(postCount, _contentCID);
     }
 
     function proposeConnect(address _otherProfile) external onlyOwners {
         require(!isProposedConnection(_otherProfile), "Connection already proposed");
         // Call externalProposeConnect on the other profile contract
         // Passing this contract's address for approval/denial
+        externalProposeConnect(_otherProfile);
     }
 
     function externalProposeConnect(address _proposingProfile) external onlyOwners {
         // Implementation to handle external connection proposal
+
     }
 
     function listProposedConnections() external view returns (address[] memory) {
         // Return array of proposed connection addresses
+        // Return empty array if no proposed connections
+        return proposedConnections[msg.sender];
     }
 
     function isProposedConnection(address _profile) public view returns (bool) {
         // Check if the given address is in the list of proposed connections
         // Return true if proposed, false otherwise
+        return proposedConnections[msg.sender][_profile];
     }
 }
