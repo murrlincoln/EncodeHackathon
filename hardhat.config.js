02a2fba1d09c23995a9145e60d4e6b051c9fb0b5
justinyeh1995 <justinyeh1995@gmail.com>
2023-08-26 17:49:00 -0500

    update default network to sepolia

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,14 +1,21 @@
 require("@nomicfoundation/hardhat-toolbox");
-
+require("dotenv").config();
+const { API_URL, PRIVATE_KEY } = process.env;
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.9",
+  defaultNetwork: "sepolia",
   paths: {
     artifacts: "./src/artifacts",
   },
   networks: {
     hardhat: {
-      chainId: 1337,
+      chainId: 1337, // Local Hardhat network
     },
+    sepolia: {
+      url: API_URL,
+      accounts:  [PRIVATE_KEY]
+    }
   },
+
 };
